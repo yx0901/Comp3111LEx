cc536f4c76b333ea8f8d100b19119ca1830cf0d6
Yuxuan Lin <yl2886@cornell.edu>
2025-02-24 16:13:12 +0800

    Implemented Lab2

---
+++ src/main/java/Lab2b/Book.java
@@ -0,0 +1,33 @@
+package Lab2b;
+
+import java.util.Arrays;
+
+public class Book {
+    private String chapters[];
+
+    private static final int DEFAULT_CHAPTERS = 10;
+    public Book () {
+        chapters = new String[DEFAULT_CHAPTERS];
+        Arrays.fill(chapters, "n/a");
+
+    }
+
+    public Book (String[] argument) {
+        /* construct the object with an array of chapter names
+        /* PLEASE ADD YOUR CODE HERE */
+        chapters = new String[argument.length];
+        for (int i = 0; i < argument.length; i++) {
+            chapters[i] = argument[i];
+        }
+    }
+
+    public String getChapter (int i) {
+        /* return the chapter by the given index */
+        /* PLEASE ADD YOUR CODE HERE */
+        return chapters[i];
+    }
+
+    public String[] getChapters () {
+        return chapters;
+    }
+}
---
+++ src/main/java/Lab2b/mainApp2b.java
@@ -0,0 +1,18 @@
+package Lab2b;
+
+import java.util.Arrays;
+
+public class mainApp2b {
+    public static void main(String[] args) {
+        final String array[] = {"Basic Java", "Advanced Java", "Guru Java"};
+        Book b = new Book(array);
+        int k =2;
+        System.out.println("The title of Chapter " + k + " is: " + b.getChapter(k-1));
+        String anotherArray[] = b.getChapters();
+
+        System.out.println("There are " + anotherArray.length + " chapters");
+        System.out.println(java.util.Arrays.toString(anotherArray));
+
+
+    }
+}
